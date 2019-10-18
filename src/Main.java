efe2d184de9169abe5ce1c9d5ed5589c145995a2
Wenbo <wenbo.xiao@student.adelaide.edu.au>
2019-10-18 10:48:12 +1030

    add ThreeSunCloset solution

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,18 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        List<List<Integer>> res = ThreeSumDev.threeSum(new int[]{-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6});
+//        List<List<Integer>> res = ThreeSumDev.threeSum(new int[]{-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6});
+//
+//        for (int i = 0; i < res.size(); i++) {
+//            for (int j = 0; j < res.get(i).size(); j++) {
+//                System.out.println(res.get(i).get(j));
+//            }
+//        }
 
-        for (int i = 0; i < res.size(); i++) {
-            for (int j = 0; j < res.get(i).size(); j++) {
-                System.out.println(res.get(i).get(j));
-            }
-        }
 
+    int res = ThreeSumCloset.threeSumClosest(new int[]{-1, 2, 1, -4}, 1);
+
+        System.out.println(res);
 
 
     }
--- src/ThreeSumCloset.java
+++ src/ThreeSumCloset.java
@@ -7,36 +7,80 @@ import java.util.List;
  * Created by Lance on 10/17/2019.
  */
 public class ThreeSumCloset {
-    public int threeSumClosest(int[] nums, int target) {
-        int left = 1;
-        int right = nums.length-1;
-        int res = 0;
+    /** 16. 3SumCloset
+     * Given an array nums of n integers and an integer target,
+     * find three integers in nums such that the sum is closest to target.
+     * Return the sum of the three integers.
+     * You may assume that each input would have exactly one solution.
+     *
+     * Example:
+     * Given array nums = [-1, 2, 1, -4], and target = 1.
+     *
+     * The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+     *
+     * time O(n^2)
+     * space O(n)
+     *
+     * @param
+     * @return
+     */
 
-        Arrays.sort(nums);
-        //-4,-1,1,2
-        for (int i = 0; i < nums.length; i++) {
-            int c = target - nums[i];
-            while(left < right){
-//                if(c == nums[left] + nums[right]){
+    // write by myself
+//        public static int threeSumClosest(int[] nums, int target) {
 //
 //
+//            int res = 0;
+//            int min = Integer.MAX_VALUE;
+//            int actMin ;
 //
+//            Arrays.sort(nums);
+//            //-4,-1,1,2
+//            for (int i = 0; i < nums.length - 2; i++) {
+//                int c = target - nums[i];
+//                int left = i +1;
+//                int right = nums.length-1;
+//                while(left < right) {
+//                    if(c == nums[left] + nums[right]) {
+//                        // if diff is 0, return target.
+//                        return target;
+//                    }
+//                    if (Math.abs(c - nums[left] - nums[right]) < min) {
+//                        min = Math.abs(c - nums[left] - nums[right]);
+//                        actMin = c - nums[left] - nums[right];
+//                        res = target - actMin;
 //
-//
-//                }else if(c > nums[left] + nums[right]){
-//                    left++;
-//                }else{
-//                    right--;
+//                    }
+//                    if(c > nums[left] + nums[right]){
+//                        left++;
+//                    }else{
+//                        right--;
+//                    }
 //                }
+//
+//            }
+//
+//            return res;
+//        }
 
-                
+    public static int threeSumClosest(int[] nums, int target) {
+        int res = nums[0] + nums[1] + nums[nums.length - 1];
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length-2; i++) {
+            //reset left pointer for each loop
+            int left = i + 1, right = nums.length - 1;
+            while (left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum > target){
+                    right--;
+                }else left++;
+                // find the minimum difference between target and current cum
+                if (Math.abs(sum - target) < Math.abs(res - target)){
+                    res = sum;
+                }
             }
-
-
-
-
         }
-
         return res;
     }
+
+
 }
